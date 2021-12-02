a306ae714fe7ff80b3d1960fe1f1183a4ce49c85
Jeremy Durden <jeremydurden@gmail.com>
2021-12-02 12:40:43 -0800

    beginning card component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./style.css";
 //Components
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Card />
     </>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,20 @@
+import star from "../Images/Star.svg";
+import image from "../Images/katie.svg";
+
+function Card() {
+  return (
+    <div className="card--container">
+      <img src={image} alt="" className="card--image" />
+      <img src={star} alt="" className="card--star" />
+      <p className="card--rating">
+        5.0 <span className="card--rating-span"></span>{" "}
+      </p>
+      <h3 className="card--title">Life Lessons with Katie Zaferes</h3>
+      <p className="card--cost">
+        <span className="card--cost-span">From $136</span> / person
+      </p>
+    </div>
+  );
+}
+
+export default Card;
