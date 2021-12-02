4e6d0e753efafe7b19bedb295025ef48b615964d
Jeremy Durden <jeremydurden@gmail.com>
2021-12-01 20:33:09 -0800

    styling navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,9 +2,9 @@ import logo from "../Images/airbnb 1.svg";
 
 function Navbar() {
   return (
-    <div className="nav--container">
+    <nav className="nav--container">
       <img src={logo} alt="air-bnb-logo" className="nav--logo" />
-    </div>
+    </nav>
   );
 }
 
