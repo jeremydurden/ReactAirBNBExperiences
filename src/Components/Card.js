58a45e4371e2f54f3a386889bad0739a327c8957
Jeremy Durden <jeremydurden@gmail.com>
2021-12-05 14:21:51 -0800

    card styling

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,7 +7,7 @@ function Card() {
       <img src={image} alt="" className="card--image" />
       <img src={star} alt="" className="card--star" />
       <p className="card--rating">
-        5.0 <span className="card--rating-span"></span>{" "}
+        5.0 <span className="card--rating-span">(6) · USA</span>
       </p>
       <h3 className="card--title">Life Lessons with Katie Zaferes</h3>
       <p className="card--cost">
